697df5ac3ce87ad1731ffdd518e128d2dc24f259
yjfyeyu@gmail.com <yjfyeyu@gmail.com@c1ecae70-c3b2-6979-4077-91f6ff730283>
2013-06-04 13:21:17 +0000

    2013.6.4	Ver 0.15 1、增加保存设置功能（为定时自动执行任务做准备）。

--- trunk/ glq --username yjfyeyu@gmail.com/pvpgn glq/Form1.Designer.vb
+++ trunk/ glq --username yjfyeyu@gmail.com/pvpgn glq/Form1.Designer.vb
@@ -160,7 +160,7 @@ Partial Class Form1
         Me.Label8.Name = "Label8"
         Me.Label8.Size = New System.Drawing.Size(107, 12)
         Me.Label8.TabIndex = 22
-        Me.Label8.Text = "Ver 0.14 build 21"
+        Me.Label8.Text = "Ver 0.15 build 23"
         '
         'Label9
         '
